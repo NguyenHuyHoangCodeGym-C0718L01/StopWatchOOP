ccffbea2a3e0b75ad636e697fa084b2fac94dcce
Nguyen Huy Hoang <huyhoang681997@gmail.com>
2018-10-01 21:19:50 +0700

    Hoang finish 'Stop Watch OOP' project

---
+++ src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int[] arr = new int[10000];
+        Random random = new Random(1000000);
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = random.nextInt();
+        }
+        StopWatch stopWatch = new StopWatch();
+        long startTime = stopWatch.start();
+        selectionSort(arr);
+        long endTime = stopWatch.end();
+        System.out.println(stopWatch.getElapsedTime(startTime, endTime));
+    }
+
+    public static void selectionSort(int[] arr){
+        for (int i = 0; i < arr.length - 1; i++)
+        {
+            int index = i;
+            for (int j = i + 1; j < arr.length; j++){
+                if (arr[j] < arr[index]){
+                    index = j;//searching for lowest index
+                }
+            }
+            int smallerNumber = arr[index];
+            arr[index] = arr[i];
+            arr[i] = smallerNumber;
+        }
+    }
+
+}
